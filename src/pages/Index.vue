3509ed5718ee6b875a6aff48408e7bc7f5a8c34e
Christos Alexiou <christos.alexiou@alfatraining.gr>
2021-11-14 00:27:03 +0200

    Make button visible after upload

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,60 +1,72 @@
 <template>
-<q-page class="column items-center justify-center">
-    <q-img src="lora.png" spinner-color="white" style="height: 100px; max-width: 100px" />
+  <q-page class="column items-center justify-center">
+    <q-img
+      src="lora.png"
+      spinner-color="white"
+      style="height: 100px; max-width: 100px"
+    />
     <h2 class="text-center" style="font-family: 'Mochiy Pop P One', sans-serif">
-        lora
+      lora
     </h2>
     <p style="font-family: 'Mochiy Pop P One', sans-serif">
-        Asynchronous Transcription Service
+      Asynchronous Transcription Service
     </p>
     <div class="q-pa-md">
-        <div class="q-gutter-sm row items-start">
-            <q-uploader accept="audio/*" auto-upload url="http://localhost:4444/upload" style="max-width: 300px" />
-        </div>
+      <div class="q-gutter-sm row items-start">
+        <q-uploader
+          @uploaded="this.stateAnalyzeBtn = true"
+          accept="audio/*"
+          auto-upload
+          url="http://localhost:4444/upload"
+          style="max-width: 300px"
+        />
+      </div>
     </div>
     <div class="q-pa-md q-gutter-sm">
-        <q-btn disable @click="onClick" color="primary" label="analyze" />
+      <q-btn
+        v-if="this.stateAnalyzeBtn"
+        @click="onClick"
+        color="primary"
+        label="analyze"
+      />
     </div>
-</q-page>
+  </q-page>
 </template>
 
 <script>
-import {
-    api
-} from 'boot/axios';
+import { api } from "boot/axios";
 
 export default {
-    data() {
-        return {}
-    },
-    methods() {
-        return {}
-    },
-    setup() {
-        return {
-            onClick() {
-                api.get('/analyze').then(response => {
-                    if (response.status == 200) {
-                        console.log(response.data)
-                    } else {
-                        console.log('something went wrong');
-                    }
-                })
-
-            }
-        }
-
-    }
-
+  data() {
+    return {
+      stateAnalyzeBtn: false,
+    };
+  },
+  methods() {
+    return {};
+  },
+  setup() {
+    return {
+      onClick() {
+        api.get("/analyze").then((response) => {
+          if (response.status == 200) {
+            console.log(response.data);
+          } else {
+            console.log("something went wrong");
+          }
+        });
+      },
+    };
+  },
 };
 </script>
 
 <style scoped>
 h2 {
-    margin-bottom: 5px;
+  margin-bottom: 5px;
 }
 
 subtitle {
-    margin-top: 0px;
+  margin-top: 0px;
 }
 </style>
